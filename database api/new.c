02d6a649fae339fc342bedf9defb01b15fe7e706
coreliyan <1378139998@qq.com>
2018-03-10 15:17:15 +0800

    new2.0

--- database api/new.c
+++ database api/new.c
@@ -1,4 +1,5 @@
 int creatNum = 0;
+char creatNumStr[10];
 
 /*******************************************************************************
  * 名称: 
@@ -24,37 +25,36 @@ int get_next_qqNum_callback(void *data, int col_count, char **col_values, char *
  * 说明: 无
  * 使用: int tmp = get_next_qqNum()
 *******************************************************************************/
-int get_next_qqNum(void)
+char *get_next_qqNum(void)
 {
 	int err;
 	char *pErrMsg = 0;
 	char sql[100] = "select * from qqNum_table";
-	char str[10];
 
 	if (SQLITE_OK != (err = sqlite3_open("database.db", &db))) {				// 打开数据库
 	
 		printf("创建数据库 %s 失败(%s)\n", "database.db", sqlite3_errstr(err));
-		return -1;
+		return NULL;
 	}
 	
 	err = sqlite3_exec(db, sql, get_next_qqNum_callback, 0, &pErrMsg);
     if (err != SQLITE_OK) {
     
 		sqlite3_close(db);
-        return -1;
+        return NULL;
     }
 	
 	creatNum += 1;																// 读取 QQNum + 1, 写回 qqNum_table
-	sprintf(str, "%d", creatNum);
-	sprintf(sql, "update qqNum_table set qqNum = '%s';", str);
+	sprintf(creatNumStr, "%d", creatNum);
+	sprintf(sql, "update qqNum_table set qqNum = '%s';", creatNumStr);
 	err = sqlite3_exec(db, sql, NULL, NULL, &pErrMsg);
     if (err != SQLITE_OK) {
 
 		sqlite3_close(db);
-        return -1;
+        return NULL;
     }
 	
 	sqlite3_close(db);
 	
-	return creatNum;
+	return creatNumStr;
 }
